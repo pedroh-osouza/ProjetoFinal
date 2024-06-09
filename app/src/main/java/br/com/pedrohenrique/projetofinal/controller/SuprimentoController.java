cd847d336ba5097046c41798cb804bd2333141c6
Pedro Souza <pedroh.olives@gmail.com>
2024-06-09 11:44:46 -0400

    na crescente

--- app/src/main/java/br/com/pedrohenrique/projetofinal/controller/SuprimentoController.java
+++ app/src/main/java/br/com/pedrohenrique/projetofinal/controller/SuprimentoController.java
@@ -22,17 +22,7 @@ public class SuprimentoController {
     }
 
     public Suprimento consultar(String uid) {
-        CollectionReference referencia = db.collection("suprimentos");
-        referencia.document(uid).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        Log.d(TAG, document.getId() + " => " + document.getData());
-                    }
-                }
-            }
-        });
+        return null;
 
     }
 
--- app/src/main/java/br/com/pedrohenrique/projetofinal/view/MainActivity.java
+++ app/src/main/java/br/com/pedrohenrique/projetofinal/view/MainActivity.java
@@ -3,12 +3,47 @@ package br.com.pedrohenrique.projetofinal.view;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import br.com.pedrohenrique.projetofinal.R;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnRegister;
+    private Button btnGuest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnGuest = findViewById(R.id.btnGuest);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        btnGuest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent para a tela de convidado, substitua GuestActivity pela sua activity de convidado
+                //Intent guestIntent = new Intent(MainActivity.this, GuestActivity.class);
+                //startActivity(guestIntent);
+            }
+        });
 
     }
 }
